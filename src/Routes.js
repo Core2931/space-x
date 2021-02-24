c8d5a544f2c6edf812d88ba24fc1bfb4d2281d7f
Core2931 <mark.2631@hotmail.com>
2021-02-24 16:32:16 +0700

    de

--- src/Routes.js
+++ src/Routes.js
@@ -18,7 +18,7 @@ export default class Routes extends Component {
                     <Route path="/space-x/Home" exact component={Home} />
                     <Route path="/space-x/Rocket" component={Rocket} />
                     <Route path="/space-x/Launch" component={Launch} />
-                    <Route path="/space-x/Rocketdetail/" component={Rocketdetail} />
+                    <Route path="/space-x/Rocket/:rokcet_id" component={Rocketdetail} />
                     <Route path="/space-x/Launch/:flight_number" component={Launchdetail} />
                 </Switch>
             </Router>
